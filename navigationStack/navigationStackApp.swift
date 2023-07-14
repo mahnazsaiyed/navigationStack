e60f49afba181c2fccd063066266857d10272571
mahnazsaiyed <mahnazsaiyed@gmail.com>
2023-07-14 09:21:21 -0700

    Initial Commit

---
+++ navigationStack/navigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigationStackApp.swift
+//  navigationStack
+//
+//  Created by Scholar on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct navigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
